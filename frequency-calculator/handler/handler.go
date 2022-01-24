e78c3d995d106b076057064104c8f19d1069dce8
Prithvi Chouhan <pchouhan@deqode.com>
2022-01-24 09:35:58 +0530

    Create: function to calculate input word frequency

--- frequency-calculator/handler/handler.go
+++ frequency-calculator/handler/handler.go
@@ -10,6 +10,15 @@ import (
 	errors "frequency-calculator/shared/error"
 )
 
+type request struct {
+	Input string
+}
+
+type wordFrequency struct {
+	Word      string
+	Frequency int
+}
+
 // Function to validate input
 func validateInput(input string) (string, error) {
 	if input == "" {
@@ -25,4 +34,44 @@ func printHeader(w http.ResponseWriter, code int, result interface{}) {
 	w.WriteHeader(code)
 	post, _ := json.Marshal(result)
 	fmt.Fprintf(w, "%v", string(post))
+}
+
+// Function to calculate input words frequency
+func CalculateWordsFrequency(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var body request
+	var topTenEle []wordFrequency
+	_ = json.NewDecoder(r.Body).Decode(&body)
+
+	_, err := validateInput(body.Input)
+	if err != nil {
+		printHeader(w, http.StatusBadRequest, err)
+		return
+	}
+
+	input := strings.Fields(body.Input)
+	wordMap := make(map[string]int)
+
+	for _, v := range input {
+		_, ok := wordMap[v]
+		if ok {
+			wordMap[v] += 1
+		} else {
+			wordMap[v] = 1
+		}
+	}
+
+	for k, v := range wordMap {
+		topTenEle = append(topTenEle, wordFrequency{k, v})
+	}
+
+	sort.Slice(topTenEle, func(i, j int) bool {
+		return topTenEle[i].Frequency > topTenEle[j].Frequency
+	})
+
+	if len(topTenEle) > 10 {
+		topTenEle = topTenEle[:10]
+	}
+
+	json.NewEncoder(w).Encode(topTenEle)
 }
