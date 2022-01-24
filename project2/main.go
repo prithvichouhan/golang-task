5a974c3c823e8efbd20214c36f2de9542ca5f919
Prithvi Chouhan <pchouhan@deqode.com>
2022-01-24 17:13:56 +0530

    Add: function to call API and get top ten element

--- project2/main.go
+++ project2/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"os"
 
 	"github.com/joho/godotenv"
 )
+
 // Function init
 func init() {
 	err := godotenv.Load(".env")
@@ -12,4 +19,35 @@ func init() {
 	if err != nil {
 		log.Fatal("Error: Unable to load env")
 	}
+}
+
+// Function to get the top ten element
+func getTopTenElement(input string) {
+	//Encode the data
+	postBody, _ := json.Marshal(map[string]string{
+		"input": input,
+	})
+
+	body := bytes.NewBuffer(postBody)
+	resp, err := http.Post(os.Getenv("URL"), "application/json", body)
+	if err != nil {
+		log.Fatalf("An Error Occured %v", err)
+	}
+
+	responseData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("An Error Occured %v", err)
+	}
+
+	fmt.Println("Top Ten element - ", string(responseData))
+}
+
+// Function Main
+func main() {
+	content, err := ioutil.ReadFile("data/input.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	getTopTenElement(string(content))
 }
