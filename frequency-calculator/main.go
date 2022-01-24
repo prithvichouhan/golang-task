59e51df3c304d499a7a6c735d167cede822138d4
Prithvi Chouhan <pchouhan@deqode.com>
2022-01-24 09:37:56 +0530

    Create: init to load env

--- frequency-calculator/main.go
+++ frequency-calculator/main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	handler "frequency-calculator/handler"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error: Unable to load env")
+	}
+}
 
 func initializeRouter() {
 	r := mux.NewRouter()
